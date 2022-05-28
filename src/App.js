73e9f3a5631f9e4b36f693c98f5dfbc841aa9da5
DivineFavour <edehdivine042@gmail.com>
2022-05-28 18:57:31 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className='app'>
       <Home />
+      <h1 style={{marginTop: "50px", padding: "10px"}}>Made with ❤️ by <a href='https://twitter.com/divine_edeh1'>Divne Edeh</a></h1>
     </div>
   );
 }
